1fb089f930b827ab9e632bd4a182c98a535eac11
calliereimann <calliereimann5@gmail.com>
2024-07-16 16:10:43 -0700

    finishing part 2 of the day

--- day3.py
+++ day3.py
@@ -33,6 +33,7 @@ def dictify(data):
         index +=1 
     return output #converting the original data into a list of tuples, one per row of the initial input - each tuple has a numbers chunk and a symbols chunk
 
+
 #Part 1 code
 
 #My first idea is to scan the data into a 2d matrix, comparing coordinates for each number, but that would be absurdly computationally expensive. Not impossible by any means, but deeply painful. Maybe scanning as tuples? Line number, head, length, value? Where Value is either an int or a symbol. Maybe list of things considered 'valid' symbols. Dictionary of Dictionaries, maybe? Each row is a key for the first dictionary, then the column is the secondary key? numbers never wrap, so we can give them clear and precise row numbers, too
@@ -47,14 +48,14 @@ def process(line, lastline, nextline = None):
     numbers, symbols = line
     score = 0
     for number in numbers: #in a dictionary, this scans over the keys, not the values, which is fortunate for us
-        len, value = numbers[number]
+        length, value = numbers[number]
         include = False
-        if (number-1) in symbols or (number+len) in symbols:
+        if (number-1) in symbols or (number+length) in symbols:
             include = True
         if lastline != None and not include:
             discard, lastsymbols = lastline
             check = -1
-            while check <= len and not include: #for efficiency, the moment include becomes true we stop looping and skip any remaining checks
+            while check <= length and not include: #for efficiency, the moment include becomes true we stop looping and skip any remaining checks
                 if (number+check) in lastsymbols:
                     print(str(value) + " passed lastsymbol check at " + str(number+check))
                     include = True
@@ -63,7 +64,7 @@ def process(line, lastline, nextline = None):
         if nextline != None and not include:
             discard, nextsymbols = nextline
             check = -1
-            while check <= len and not include:
+            while check <= length and not include:
                 if (number+check) in nextsymbols:
                     print(str(value) + " passed nextsymbol check at " + str(number+check))
                     include = True
@@ -89,7 +90,45 @@ def mainfunc(data):
 
 #Part 2 code
 
+def gearcheck(line, lastline, nextline = None):
+    numbers, symbols = line
+    score = 0
+    for symbol in symbols: #in a dictionary, this scans over the keys, not the values, which is fortunate for us
+        if symbols[symbol] != '*':
+            continue
+        values = []
+        for number in numbers:
+            length, value = numbers[number]
+            if number == symbol+1 or number+length == symbol:
+                values.append(value)
+        if lastline != None:
+            lastnumbers, discard = lastline
+            for lastnumber in lastnumbers:
+                length, value = lastnumbers[lastnumber]
+                if lastnumber == symbol+1 or lastnumber+length == symbol or (lastnumber <= symbol and lastnumber+length >= symbol): #corner, other corner, and any form of direct contact
+                    values.append(value)
+        if nextline != None:
+            nextnumbers, discard = nextline
+            for nextnumber in nextnumbers:
+                length, value = nextnumbers[nextnumber]
+                if nextnumber == symbol+1 or nextnumber+length == symbol or (nextnumber <= symbol and nextnumber+length >= symbol): #corner, other corner, and any form of direct contact
+                    values.append(value)
+        if len(values) == 2:
+            score += values[0] * values[1]
+    return score
 
+def gearfunc(data):
+    list = dictify(data)
+    score = gearcheck(list[0], None, list[1])
+    print(score)
+    index = 1
+    while index < (len(list)-1):
+        val = gearcheck(list[index], list[index-1], list[index+1])
+        score += val
+        print(str(val) + ', ' + str(score))
+        index +=1 
+    score += gearcheck(list[-1], list[-2])
+    return score    
 
 class Tests(unittest.TestCase):
     def test_linescan(self):
@@ -106,9 +145,20 @@ class Tests(unittest.TestCase):
 ......755.
 ...$.*....
 .664.598..""") == 4361 #oh im so frustrated by the way that multiline quotes interact with tabs - it makes perfect sense im just really frustrated
+    def test_mainpart2(self):
+        assert gearfunc("""467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""") == 467835
     
 with open("day3.txt") as f:
    data = f.read()#
 
 if __name__ == "__main__":
-    print(mainfunc(data))
+    print(gearfunc(data))
