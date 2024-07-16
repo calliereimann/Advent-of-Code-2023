50b5071cec65a0f85c5d7d18218a6d8fdb4858cb
calliereimann <calliereimann5@gmail.com>
2024-07-16 14:16:08 -0700

    finishing part1

--- day3.py
+++ day3.py
@@ -8,6 +8,7 @@ def scanline(line):
     symbols = {}
     numbers = {}
     loopmax = len(line)
+    print("loop length is " + str(loopmax))
     index = 0 
     while index < loopmax:
         if line[index] == ".":
@@ -16,14 +17,16 @@ def scanline(line):
         elif line[index].isdecimal():
             check = 1 #improved version of original idea, can handle arbitrarily long numbers and doesnt need lengthy if/else chains
             #technically overbuilt for what we need since the maximum digit length in the data is 3, but it's cleaner
-            while line[index+check].isdecimal():
+            while index+check < loopmax and line[index+check].isdecimal():
                 check +=1
             numbers[index] = (check, int(line[index:index+check]))
             index += check
         else:
             symbols[index] = line[index]
             index +=1
-    return (numbers, symbols)
+    output = (numbers, symbols)
+    print(output)
+    return output
 
 #but the problem now is how to efficiently scan through the dictionaries for things nearby each other - well, i guess i don't actually care about efficiency as long as things are correct? but it just grates at me to have something that burns a ton of processing power
 
@@ -36,7 +39,7 @@ def dictify(data):
         index +=1 
     return output #converting the original data into a list of tuples, one per row of the initial input - each tuple has a numbers chunk and a symbols chunk
 
-def process(line, lastline = None, nextline = None):
+def process(line, lastline, nextline = None):
     numbers, symbols = line
     score = 0
     for number in numbers: #in a dictionary, this scans over the keys, not the values, which is fortunate for us
@@ -48,7 +51,8 @@ def process(line, lastline = None, nextline = None):
             discard, lastsymbols = lastline
             check = -1
             while check <= len and not include: #for efficiency, the moment include becomes true we stop looping and skip any remaining checks
-                if number+check in lastsymbols:
+                if (number+check) in lastsymbols:
+                    print(str(value) + " passed lastsymbol check at " + str(number+check))
                     include = True
                 else:
                     check +=1 
@@ -56,20 +60,28 @@ def process(line, lastline = None, nextline = None):
             discard, nextsymbols = nextline
             check = -1
             while check <= len and not include:
-                if number+check in nextsymbols:
+                if (number+check) in nextsymbols:
+                    print(str(value) + " passed nextsymbol check at " + str(number+check))
                     include = True
                 else:
                     check +=1 
         if include:
+            print("including: " + str(value))
             score += value
     return score
 
 def mainfunc(data):
     list = dictify(data)
     score = process(list[0], None, list[1])
+    print(score)
     index = 1
-    while index < len(list):
-        score += process(list[index], list[index-1], list[index+1])
+    while index < (len(list)-1):
+        val = process(list[index], list[index-1], list[index+1])
+        score += val
+        print(str(val) + ', ' + str(score))
+        index +=1 
+    score += process(list[-1], list[-2])
+    return score    
 
 #Part 2 code
 
@@ -79,10 +91,20 @@ class Tests(unittest.TestCase):
     def test_linescan(self):
         assert scanline("...23%..154..@...876...!@") == ({3:(2, 23), 8:(3,154), 17:(3,876)}, {5:'%', 13:'@', 23:'!', 24:'@'})
         
-    
+    def test_mainpart1(self):
+        assert mainfunc("""467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""") == 4361 #oh im so frustrated by the way that multiline quotes interact with tabs - it makes perfect sense im just really frustrated
     
 with open("day3.txt") as f:
    data = f.read()#
 
 if __name__ == "__main__":
-    pass
+    print(mainfunc(data))
