9884132bd00bc25eff6746b1cf6432e1ac7bd3f2
calliereimann <calliereimann5@gmail.com>
2024-07-20 16:19:24 -0700

    completing part 1, first thoughts on part 2

--- day4.py
+++ day4.py
@@ -6,20 +6,50 @@ import unittest
 #so we can use substring tricks to make things a lot easier
 #at the cost of having 'magic numbers', but what can ya do
 
+def doublezerofilter(line):
+    index = line.find("  ") #semi efficiently strips out double zeroes so that the split method in linesplit doesnt create '' values 
+    while index != -1:
+        line = line[:index] + line[index+1:]
+        index = line.find("  ")
+    return line
+
 def linesplit(line):
     cardid = line[0:9]
-    winlist = line[9:40].strip().split(" ")
-    mylist = line[42:]
-    print (cardid,winlist,mylist)
+    winlist = doublezerofilter(line[9:40].strip()).split(" ")
+    mylist = doublezerofilter(line[42:].strip()).split(" ")
+    return (cardid,winlist,mylist)
 
-linesplit("Card   5: 46 50 28 25 44 73  3 14 17 20 | 33 14 44 71 73 59 19 80 40 20  5  6 72 85 47 62 30 50 83 51 24 28  3 77 39")
+def cardcheck(winlist, mylist):
+    score = 0
+    for item in mylist:
+        if item in winlist:
+            if score ==0:
+                score = 1
+            else:
+                score *= 2
+    return score
+
+def part1body(data):
+    list = data.splitlines()
+    score = 0
+    for item in list:
+        (discard, winlist, mylist) = linesplit(item)
+        score += cardcheck(winlist, mylist)
+    return score
+
+#we're in part 2 now, and the card numbers matter a lot, so its a good thing we're saving them 
 
 class Tests(unittest.TestCase):
-    def firsttest(self):
-        pass
+    def test_cardcheck(self):
+        assert cardcheck((41,48,83,86,17), (83,86,  6, 31, 17,  9, 48, 53)) == 8
+        assert cardcheck((13, 32, 20, 16, 61),(61, 30, 68, 82, 17, 32, 24, 19)) == 2
+        assert cardcheck((1, 21, 53, 59, 44),(69, 82, 63, 72, 16, 21, 14,  1)) == 2
+        assert cardcheck((41, 92, 73, 84, 69),(59, 84, 76, 51, 58,  5, 54, 83)) == 1
+        assert cardcheck((87, 83, 26, 28, 32),(88, 30, 70, 12, 93, 22, 82, 36)) == 0
+
 
 with open("day4.txt") as f:
    data = f.read()#
 
 if __name__ == "__main__":
-    pass
+    print(part1body(data))
