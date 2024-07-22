31e3afaac43e5f7cbc4ad487d58ca2fbef5a5f4c
calliereimann <calliereimann5@gmail.com>
2024-07-21 20:03:58 -0700

    begin day5 and creating blank template

---
+++ day5.py
@@ -0,0 +1,14 @@
+import unittest 
+
+
+
+class Tests(unittest.TestCase):
+    def test(self):
+        pass
+
+
+with open("day5.txt") as f:
+   data = f.read()#
+
+if __name__ == "__main__":
+    pass
---
+++ template.py
@@ -0,0 +1,12 @@
+import unittest 
+
+class Tests(unittest.TestCase):
+    def test(self):
+        pass
+
+
+with open("template") as f:
+   data = f.read()#
+
+if __name__ == "__main__":
+    pass
